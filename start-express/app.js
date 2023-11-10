93e7c5f43fbe127d60594e56d243df6e9b04edf3
saiashish8364 <polusaiashish97@gmail.com>
2023-11-10 16:56:17 +0530

    bodyParser

--- start-express/app.js
+++ start-express/app.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"/><input type="number" name="size"/><button type="submit">Add</button></form>'
+  );
+});
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-app.use((req, res, next) => {
-  console.log("In another middleware!");
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express!</h1>");
 });
 
