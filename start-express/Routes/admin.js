67e985e21ccfa8127eadad48c73103a2ae152e0c
saiashish8364 <polusaiashish97@gmail.com>
2023-11-28 16:08:37 +0530

    routes1

---
+++ start-express/Routes/admin.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const Router = express.Router();
+Router.get("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"/><input type="number" name="size"/><button type="submit">Add</button></form>'
+  );
+});
+Router.post("/add-product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+module.exports = Router;
